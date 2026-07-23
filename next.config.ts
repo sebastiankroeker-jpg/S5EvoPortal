a82a224472d4c7d41781b8641aeccf8954efe342
Sebastian <239161459+sebastiankroeker-jpg@users.noreply.github.com>
2026-07-23 12:23:44 +0000

    Allow map geolocation permission

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+            value: "camera=(), microphone=(), geolocation=(self), interest-cohort=()",
           },
         ],
       },
